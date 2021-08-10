64e2f528da8776e47f5a7aac1d0e77f5817f37a6
xBrandonGx97 <xBrandonGx97@gmail.com>
2021-08-10 16:17:30 -0500

    small chanbges

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".nav-menu");
+
+    hamburger.addEventListener("click", mobileMenu);
+
+    function mobileMenu() {
+        hamburger.classList.toggle("active");
+        navMenu.classList.toggle("active");
+    }
+});
