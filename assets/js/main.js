5190210a009fa189d11cdaa4b6788aeb7940bebe
xBrandonGx97 <xBrandonGx97@gmail.com>
2021-08-18 15:45:33 -0500

    removed body scroll when mobile nav open

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,20 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     const hamburger = document.querySelector(".hamburger");
+//     const navMenu = document.querySelector(".nav-menu");
+
+//     hamburger.addEventListener("click", mobileMenu);
+
+//     function mobileMenu() {
+//         hamburger.classList.toggle("active");
+//         navMenu.classList.toggle("active");
+//     }
+// });
 document.addEventListener('DOMContentLoaded', function () {
-    const hamburger = document.querySelector(".hamburger");
-    const navMenu = document.querySelector(".nav-menu");
-
-    hamburger.addEventListener("click", mobileMenu);
-
-    function mobileMenu() {
-        hamburger.classList.toggle("active");
-        navMenu.classList.toggle("active");
-    }
+    document.getElementById('nav-toggle').addEventListener('change', function() {
+        if (this.checked) {
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = 'auto';
+        }
+    });
 });
